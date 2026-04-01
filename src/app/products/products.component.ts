0262ccc8fbe19837b8c17bea7f2369962641d5c5
aymensellaouti <aymen.sellaouti@gmail.com>
2026-04-01 12:27:07 +0100

    products

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, inject } from "@angular/core";
 import {
   BehaviorSubject,
   Observable,
@@ -6,6 +6,7 @@ import {
   map,
   takeWhile,
   scan,
+  tap,
 } from "rxjs";
 import { Product } from "./dto/product.dto";
 import { ProductService } from "./services/product.service";
@@ -17,10 +18,21 @@ import { Settings } from "./dto/product-settings.dto";
   styleUrls: ['./products.component.css'],
 })
 export class ProductsComponent {
+  #setting: Settings = {limit: 12,skip: 0};
+  productService = inject(ProductService);
+  total = 100;
+  #settings$ = new BehaviorSubject(this.#setting);
   /* Todo : Faire le nécessaire pour créer le flux des produits à afficher */
   /* Tips : vous pouvez voir les différents imports non utilisés et vous en inspirer */
-  products$!: Observable<Product[]>;
+  products$: Observable<Product[]> = this.#settings$.pipe(
+    concatMap(setting => this.productService.getProducts(setting)),
+    tap(resultat => this.total = resultat.total),
+    map(result => result.products),
+    scan((oldProducts, newProducts) => [...oldProducts, ...newProducts]),
+    takeWhile(products => this.total > products.length)
+  );
   more() {
-
+    this.#setting.skip += this.#setting.limit;
+    this.#settings$.next(this.#setting);
   }
 }
